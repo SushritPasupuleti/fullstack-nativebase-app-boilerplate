24eac4025da2e680945784a9665a8f3599d89c99
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2022-04-26 19:23:27 +0530

    chore: add expo app
    Get one component to work across next and expo

---
+++ src/apps/expo/App.js
@@ -0,0 +1,12 @@
+import { Text } from "react-native"
+import { CrossPlatformMessage } from "ui/CrossPlatformMessage"
+
+export default function App() {
+  return (
+    <>
+      <Text>
+        <CrossPlatformMessage />
+      </Text>
+    </>
+  )
+}
--- src/apps/web/pages/index.js
+++ src/apps/web/pages/index.js
@@ -1,10 +1,13 @@
 import { Button } from "ui";
+import { CrossPlatformMessage } from "ui/CrossPlatformMessage";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
       <Button />
+      <br />
+      <CrossPlatformMessage />
     </div>
   );
 }
